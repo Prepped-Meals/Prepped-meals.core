64593d054624ee6f9ce423a034b5c0dfde3bf54d
Dulen Manthuka <dulenmanthuka2003@gmail.com>
2025-03-19 23:41:41 +0530

    Cors_Enabled_backend-dulen

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./Config/db.js";
-import customerRoutes from "./routes/customerRoutes.js"
+import customerRoutes from "./routes/customerRoutes.js";
 import seedDatabase from "./Config/seedDB.js";
-import paymentcardRoutes from "./routes/paymentCardRoutes.js"
+import paymentcardRoutes from "./Routes/paymentCardRoutes.js";
+import cors from "cors";
 
-dotenv.config(); 
+dotenv.config();
 
 // Connect to MongoDB & Seed Data
 const startServer = async () => {
-    await connectDB();
-    await seedDatabase(); 
+  await connectDB();
+  await seedDatabase();
 };
 
 startServer();
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow only frontend origin
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(express.json());
 
 app.use("/api/customers", customerRoutes);
@@ -24,4 +31,3 @@ app.use("/api/card-details", paymentcardRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
