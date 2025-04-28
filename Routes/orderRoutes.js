4feb86ff8510563b5de053e44facc2e6038a8c76
Emaya2002 <emayafernando24@gmail.com>
2025-04-28 15:09:22 +0530

    create admin order page back end

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addOrder, getOrdersByCustomer } from "../Controllers/orderController.js";
+import { addOrder, getOrdersByCustomer, getAllOrders  } from "../Controllers/orderController.js";
 
 const router = express.Router();
 
 router.post("/add-order", addOrder);
 router.get("/get-all-orders/:customerId", getOrdersByCustomer);
+
+router.get("/get-all-orders", getAllOrders);
 
 export default router;
