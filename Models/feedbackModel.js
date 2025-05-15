1ef65512967e22fbee2a25994df5562543bd0071
Samali Apeksha <samaliapeksha@gmail.com>
2025-05-16 01:02:07 +0530

    backend changes report samali

--- Models/feedbackModel.js
+++ Models/feedbackModel.js
@@ -1,11 +1,11 @@
-// feedbackModel.js
 import mongoose from "mongoose";
 
 const FeedbackSchema = new mongoose.Schema({
   feedback_id: { type: String, required: true, unique: true },
   customer: { type: mongoose.Schema.Types.ObjectId, ref: "Customer", required: true },
   feedback_description: { type: String, required: true },
-  helpful: [{ type: mongoose.Schema.Types.ObjectId, ref: "Customer" }], // NEW
+  helpful: [{ type: mongoose.Schema.Types.ObjectId, ref: "Customer" }],
+  rating: { type: Number, min: 1, max: 5 }, // NEW: Added rating field
 }, { timestamps: true });
 
 const Feedback = mongoose.model("Feedback", FeedbackSchema);
